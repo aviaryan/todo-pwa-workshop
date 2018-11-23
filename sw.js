5dc597384a81d5a48f4c9e688af9bd05ac4ce78b
Avi Aryan <avi.aryan123@gmail.com>
2018-11-23 12:42:49 +0700

    add caching fix

--- sw.js
+++ sw.js
@@ -3,6 +3,10 @@ var CACHE_NAME = 'my-site-cache-v1';
 var urlsToCache = [
 	'/',
 	'/script.js',
+	'https://unpkg.com/react@16/umd/react.development.js',
+	'https://unpkg.com/react-dom@16/umd/react-dom.development.js',
+	'https://unpkg.com/babel-standalone@latest/babel.min.js',
+	'https://fonts.googleapis.com/css?family=Roboto',
 	'/register-sw.js',
 	'/styles.css',
 	'/img/feather-sprite.svg'
